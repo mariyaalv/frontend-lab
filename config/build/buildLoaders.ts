f8fc612f6d3d70e525637c9e20a705fb900aaacc
mariyaalv <pavlovskayama.maria@yandex.ru>
2025-01-20 21:23:45 +0300

    add new theme, add react refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,38 +2,17 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import { buildCssLoader } from "./loaders/buildCssLoader";
 import { buildSvgLoader } from "./loaders/buildSvgLoader";
+import { buildBabelLoader } from "./loaders/buildBabelLoader";
+import { buildFileLoader } from "./loaders/buildFileLoader";
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-  const babelLoader = {
-    test: /\.(js|jsx|tsx|)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader",
-      options: {
-        presets: ["@babel/preset-env"],
-        plugins: [
-          [
-            "i18next-extract",
-            {
-              locales: ["ru", "en"],
-              keyAsDefaultValue: true,
-            },
-          ],
-        ],
-      },
-    },
-  };
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options;
+
+  const babelLoader = buildBabelLoader(options);
 
   const svgLoader = buildSvgLoader();
 
-  const fileLoader = {
-    test: /\.(png|jpe?g|gif|woff2|woff)$/i,
-    use: [
-      {
-        loader: "file-loader",
-      },
-    ],
-  };
+  const fileLoader = buildFileLoader();
 
   const cssLoader = buildCssLoader(isDev);
 
